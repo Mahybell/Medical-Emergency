e298ab977ba53e247fbf24559615125534f5570b
Mahybell <132063504+Mahybell@users.noreply.github.com>
2023-07-30 15:28:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const harmburger = document.querySelector(".harmburger");
-const navBar = document.querySelector(".nav-area");
-
-harmburger.addEventListener("click", () => {
-    harmburger.classList.toggle("active");
-    navBar.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-link").forEach(n => n. 
-addEventListener("click", () => {
-    harmburger.classList.remove("active");
-    navBar.classList.remove("active");
-}))
+     // For example, you can handle button clicks
+     const signUpButton = document.querySelector('.btn-signup');
+     const signInButton = document.querySelector('.btn-signin');
+ 
+     signUpButton.addEventListener('click', () => {
+       // Redirect to the sign up page when Sign Up button is clicked
+       window.location.href = 'signup.html';
+     });
+ 
+     signInButton.addEventListener('click', () => {
+       // Redirect to the sign in page when Sign In button is clicked
+       window.location.href = 'signin.html';
+     });
